bf24c4f272c48db69e3cbc047aadc9a013b8d124
nhaarish <nhaarish@rediffmail.com>
2019-01-25 13:34:13 +0530

    Events subscriber for adding access-allow-origin in response header

---
+++ web/modules/custom/d8_demo_console/src/EventSubscriber/D8DemoConsoleSubscriber.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\d8_demo_console\EventSubscriber;
+
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * d8_demo_console event subscriber.
+ */
+class D8DemoConsoleSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   */
+  public function __construct(MessengerInterface $messenger) {
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * Kernel request event handler.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   Response event.
+   */
+  public function onKernelRequest(GetResponseEvent $event) {
+    $this->messenger->addStatus(__FUNCTION__);
+  }
+
+  /**
+   * Kernel response event handler.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
+   *   Response event.
+   */
+  public function onKernelResponse(FilterResponseEvent $event) {
+    $this->messenger->addStatus(__FUNCTION__);
+    $response = $event->getResponse();
+    $response->headers->set('access-control-allow-origin', '*');
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest'],
+      KernelEvents::RESPONSE => ['onKernelResponse'],
+    ];
+  }
+
+}
